601e0e3aae764425ba2eb1976136af776f87efb0
CodeX1315 <deepakmahade777@gmail.com>
2026-03-24 19:05:12 +0530

    callback

--- ai-playlist-generator-frontend/src/callback.jsx
+++ ai-playlist-generator-frontend/src/callback.jsx
@@ -5,10 +5,18 @@ function Callback() {
   const params = new URLSearchParams(window.location.search);
   const code = params.get("code");
 
+  console.log("CODE:", code);
+
   fetch(`/.netlify/functions/token?code=${code}`)
-    .then(res => res.json())
+    .then(res => {
+      console.log("RESPONSE STATUS:", res.status);
+      return res.json();
+    })
     .then(data => {
-      console.log("ACCESS TOKEN:", data.access_token);
+      console.log("FULL DATA:", data);
+    })
+    .catch(err => {
+      console.error("FETCH ERROR:", err);
     });
 }, []);
 
